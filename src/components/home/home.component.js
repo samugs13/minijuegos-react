72e76115a872b1db1bcd731b96324e135f2aff73
samugs13 <garciasanchezsamu@gmail.com>
2021-12-18 19:49:50 +0100

    modified home and added beta home css

--- src/components/home/home.component.js
+++ src/components/home/home.component.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import '../../css/home.css';
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Hola, Bienvenido al sistema</h1>
+      <div class="home">
+        <br></br>
+        <h1 class="titulo">JUEGOS PARA TODOS</h1>
+        <p class="texto"> La mejor página de minijuegos </p>
       </div>
     );
   }
