2fed023d5a773711e0874c539f547081fe9bf413
Andres Cienfuegos <andres.acienfuegos@gmail.com>
2021-12-18 14:32:29 +0100

    language choice dropdown in navbar

--- src/App.js
+++ src/App.js
@@ -12,16 +12,27 @@ function App() {
 		<div className="App">
 			<BrowserRouter>
 				<nav className="navbar navbar-expand navbar-dark bg-dark">
-					<div className="navbar-nav mr-auto">
-						<li className="nav-item">
-							<Link to={"/"} className="nav-link">Home</Link>
-						</li>
-						<li className="nav-item">
-							<Link to={"/quiz"} className="nav-link">Quiz</Link>
-						</li>
-						<li className="nav-item">
-							<Link to={"/tictactoe"} className="nav-link">TicTacToe</Link>
-						</li>
+					<div className="container-fluid">
+						<a className="navbar-brand" href="/">Games</a>
+						<div className="navbar-nav me-auto">
+							<li className="nav-item">
+								<Link to={"/"} className="nav-link">Home</Link>
+							</li>
+							<li className="nav-item">
+								<Link to={"/quiz"} className="nav-link">Quiz</Link>
+							</li>
+							<li className="nav-item">
+								<Link to={"/tictactoe"} className="nav-link">TicTacToe</Link>
+							</li>
+						</div>
+						<div className="d-flex">
+							<div className="dropdown me-2">
+								<select defaultValue="en" className="form-select" aria-label="Select language">
+									<option value="en">English</option>
+									<option value="es">Español</option>
+								</select>
+							</div>
+						</div>
 					</div>
 				</nav>
 
