37924651fe06221a4295fc2f461852ffe7ae03ac
Andres Cienfuegos <andres.acienfuegos@gmail.com>
2021-12-18 13:44:12 +0100

    added navigation bar

--- src/App.js
+++ src/App.js
@@ -8,17 +8,31 @@ import Quiz from './components/quiz/quiz.component';
 import Tictactoe from './components/tictactoe/tictactoe.component';
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/quiz/" element={<Quiz/>}/>
-          <Route path="/tictactoe/" element={<Tictactoe/>}/>
-        </Routes>
-     </BrowserRouter>,
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<nav className="navbar navbar-expand navbar-dark bg-dark">
+					<div className="navbar-nav mr-auto">
+						<li className="nav-item">
+							<Link to={"/"} className="nav-link">Home</Link>
+						</li>
+						<li className="nav-item">
+							<Link to={"/quiz"} className="nav-link">Quiz</Link>
+						</li>
+						<li className="nav-item">
+							<Link to={"/tictactoe"} className="nav-link">TicTacToe</Link>
+						</li>
+					</div>
+				</nav>
+
+				<Routes>
+					<Route path="/" element={<Home/>}/>
+					<Route path="/quiz/" element={<Quiz/>}/>
+					<Route path="/tictactoe/" element={<Tictactoe/>}/>
+				</Routes>
+			</BrowserRouter>,
+		</div>
+	);
 }
 
 export default App;
