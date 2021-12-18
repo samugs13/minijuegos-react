cb7685bc1843f66a003fd20bc2c9b645983f12aa
Andres Cienfuegos <andres.acienfuegos@gmail.com>
2021-12-18 13:12:26 +0100

    use bootstrap css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './css/App.css';
 import { Routes, Route, Link, BrowserRouter } from 'react-router-dom';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import Home from './components/home/home.component';
 import Quiz from './components/quiz/quiz.component';
 import Tictactoe from './components/tictactoe/tictactoe.component';
@@ -7,13 +10,13 @@ import Tictactoe from './components/tictactoe/tictactoe.component';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter> 
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/quiz/" element={<Quiz/>}/>
           <Route path="/tictactoe/" element={<Tictactoe/>}/>
         </Routes>
-     </BrowserRouter>,  
+     </BrowserRouter>,
     </div>
   );
 }
