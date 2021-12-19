2a20abcc12a460d82f23968bbecdc87735c5cdc4
samugs13 <garciasanchezsamu@gmail.com>
2021-12-19 17:53:18 +0100

    added mock-data

---
+++ src/assets/mock-data.js
@@ -0,0 +1,241 @@
+exports.quizzes = [{
+    "id": 18,
+    "question": "Capital of Cyprus",
+    "answer": "Nicosia",
+    "author": {
+        "id": 1,
+        "isAdmin": true,
+        "username": "admin",
+        "accountTypeId": 0,
+        "profileId": null,
+        "profileName": null,
+        "photo": {
+            "filename": "spg072.jpg",
+            "mime": "image/jpeg",
+            "url": "https://core.dit.upm.es/uploads/91712eceb48a55c134e80c8f38e955711cbeda8d59b19192fb75f945fcb0fb60"
+        }
+    },
+    "attachment": {
+        "filename": "capitals/Nicosia.jpg",
+        "mime": "image/jpeg",
+        "url": "https://core.dit.upm.es/uploads/capitals/Nicosia.jpg"
+    },
+    "favourite": false
+},
+{
+    "id": 6,
+    "question": "Capital of Germany",
+    "answer": "Berlin",
+    "author": {
+        "id": 1,
+        "isAdmin": true,
+        "username": "admin",
+        "accountTypeId": 0,
+        "profileId": null,
+        "profileName": null,
+        "photo": {
+            "filename": "spg072.jpg",
+            "mime": "image/jpeg",
+            "url": "https://core.dit.upm.es/uploads/91712eceb48a55c134e80c8f38e955711cbeda8d59b19192fb75f945fcb0fb60"
+        }
+    },
+    "attachment": {
+        "filename": "capitals/Berlin.jpg",
+        "mime": "image/jpeg",
+        "url": "https://core.dit.upm.es/uploads/capitals/Berlin.jpg"
+    },
+    "favourite": false
+},
+{
+    "id": 5,
+    "question": "Capital of Russia",
+    "answer": "Moscow",
+    "author": {
+        "id": 1,
+        "isAdmin": true,
+        "username": "admin",
+        "accountTypeId": 0,
+        "profileId": null,
+        "profileName": null,
+        "photo": {
+            "filename": "spg072.jpg",
+            "mime": "image/jpeg",
+            "url": "https://core.dit.upm.es/uploads/91712eceb48a55c134e80c8f38e955711cbeda8d59b19192fb75f945fcb0fb60"
+        }
+    },
+    "attachment": {
+        "filename": "capitals/Moscow.jpg",
+        "mime": "image/jpeg",
+        "url": "https://core.dit.upm.es/uploads/capitals/Moscow.jpg"
+    },
+    "favourite": false
+},
+{
+    "id": 25,
+    "question": "Capital of Egypt",
+    "answer": "Cairo",
+    "author": {
+        "id": 1,
+        "isAdmin": true,
+        "username": "admin",
+        "accountTypeId": 0,
+        "profileId": null,
+        "profileName": null,
+        "photo": {
+            "filename": "spg072.jpg",
+            "mime": "image/jpeg",
+            "url": "https://core.dit.upm.es/uploads/91712eceb48a55c134e80c8f38e955711cbeda8d59b19192fb75f945fcb0fb60"
+        }
+    },
+    "attachment": {
+        "filename": "capitals/Cairo.jpg",
+        "mime": "image/jpeg",
+        "url": "https://core.dit.upm.es/uploads/capitals/Cairo.jpg"
+    },
+    "favourite": false
+},
+{
+    "id": 11,
+    "question": "Capital of Canada",
+    "answer": "Ottawa",
+    "author": {
+        "id": 1,
+        "isAdmin": true,
+        "username": "admin",
+        "accountTypeId": 0,
+        "profileId": null,
+        "profileName": null,
+        "photo": {
+            "filename": "spg072.jpg",
+            "mime": "image/jpeg",
+            "url": "https://core.dit.upm.es/uploads/91712eceb48a55c134e80c8f38e955711cbeda8d59b19192fb75f945fcb0fb60"
+        }
+    },
+    "attachment": {
+        "filename": "capitals/Ottawa.jpg",
+        "mime": "image/jpeg",
+        "url": "https://core.dit.upm.es/uploads/capitals/Ottawa.jpg"
+    },
+    "favourite": false
+},
+{
+    "id": 29,
+    "question": "Capital of Finland",
+    "answer": "Helsinki",
+    "author": {
+        "id": 1,
+        "isAdmin": true,
+        "username": "admin",
+        "accountTypeId": 0,
+        "profileId": null,
+        "profileName": null,
+        "photo": {
+            "filename": "spg072.jpg",
+            "mime": "image/jpeg",
+            "url": "https://core.dit.upm.es/uploads/91712eceb48a55c134e80c8f38e955711cbeda8d59b19192fb75f945fcb0fb60"
+        }
+    },
+    "attachment": {
+        "filename": "capitals/Helsinki.jpg",
+        "mime": "image/jpeg",
+        "url": "https://core.dit.upm.es/uploads/capitals/Helsinki.jpg"
+    },
+    "favourite": false
+},
+{
+    "id": 22,
+    "question": "Capital of Latvia",
+    "answer": "Riga",
+    "author": {
+        "id": 1,
+        "isAdmin": true,
+        "username": "admin",
+        "accountTypeId": 0,
+        "profileId": null,
+        "profileName": null,
+        "photo": {
+            "filename": "spg072.jpg",
+            "mime": "image/jpeg",
+            "url": "https://core.dit.upm.es/uploads/91712eceb48a55c134e80c8f38e955711cbeda8d59b19192fb75f945fcb0fb60"
+        }
+    },
+    "attachment": {
+        "filename": "capitals/Riga.jpg",
+        "mime": "image/jpeg",
+        "url": "https://core.dit.upm.es/uploads/capitals/Riga.jpg"
+    },
+    "favourite": false
+},
+{
+    "id": 36,
+    "question": "Capital of Italy",
+    "answer": "Rome",
+    "author": {
+        "id": 1,
+        "isAdmin": true,
+        "username": "admin",
+        "accountTypeId": 0,
+        "profileId": null,
+        "profileName": null,
+        "photo": {
+            "filename": "spg072.jpg",
+            "mime": "image/jpeg",
+            "url": "https://core.dit.upm.es/uploads/91712eceb48a55c134e80c8f38e955711cbeda8d59b19192fb75f945fcb0fb60"
+        }
+    },
+    "attachment": {
+        "filename": "capitals/Rome.jpg",
+        "mime": "image/jpeg",
+        "url": "https://core.dit.upm.es/uploads/capitals/Rome.jpg"
+    },
+    "favourite": false
+},
+{
+    "id": 34,
+    "question": "Capital of Portugal",
+    "answer": "Lisbon",
+    "author": {
+        "id": 1,
+        "isAdmin": true,
+        "username": "admin",
+        "accountTypeId": 0,
+        "profileId": null,
+        "profileName": null,
+        "photo": {
+            "filename": "spg072.jpg",
+            "mime": "image/jpeg",
+            "url": "https://core.dit.upm.es/uploads/91712eceb48a55c134e80c8f38e955711cbeda8d59b19192fb75f945fcb0fb60"
+        }
+    },
+    "attachment": {
+        "filename": "capitals/Lisbon.jpg",
+        "mime": "image/jpeg",
+        "url": "https://core.dit.upm.es/uploads/capitals/Lisbon.jpg"
+    },
+    "favourite": false
+},
+{
+    "id": 19,
+    "question": "Capital of Azerbaijan",
+    "answer": "Baku",
+    "author": {
+        "id": 1,
+        "isAdmin": true,
+        "username": "admin",
+        "accountTypeId": 0,
+        "profileId": null,
+        "profileName": null,
+        "photo": {
+            "filename": "spg072.jpg",
+            "mime": "image/jpeg",
+            "url": "https://core.dit.upm.es/uploads/91712eceb48a55c134e80c8f38e955711cbeda8d59b19192fb75f945fcb0fb60"
+        }
+    },
+    "attachment": {
+        "filename": "capitals/Baku.jpg",
+        "mime": "image/jpeg",
+        "url": "https://core.dit.upm.es/uploads/capitals/Baku.jpg"
+    },
+    "favourite": false
+}
+]
