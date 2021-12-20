2c7723bcaa4828bdb18a752e69afbbb0806a3d55
Andres Cienfuegos <andres.acienfuegos@gmail.com>
2021-12-20 20:07:48 +0100

    renamed quizzes variable

--- src/components/quiz/quiz.component.js
+++ src/components/quiz/quiz.component.js
@@ -1,28 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import LangSelector from "../lang/LangSelector.component";
-import { quizzes } from "../../assets/mock-data";
+import { quizzes as mockData } from "../../assets/mock-data";
 import Game from './Game.component'
 
 export default function Quiz() {
 
-	const [whatquiz, setQuizzes] = useState(quizzes);
+	const [quizzes, setQuizzes] = useState([]);
 	const [currentQuiz, setCurrentQuiz] = useState(0);
 	const [score, setScore] = useState(0);
 	const [finished, setFinished] = useState(false);
 	const [disabled, setDisabled] = useState(false);
 
+	useEffect(() => {
+		setQuizzes(mockData);
+	}, []);
+
 	function nextClick(){
 
-		if (whatquiz.length==0 || currentQuiz==9)
-		setDisabled(true)
+		if (quizzes.length==0 || currentQuiz==9) setDisabled(true)
 
 		else setCurrentQuiz(currentQuiz + 1);
 	}
 
 	function previousClick(){
 
-		if (whatquiz.length==0 || currentQuiz==0)
-		setDisabled(true)
+		if (quizzes.length==0 || currentQuiz==0) setDisabled(true)
 
 		else setCurrentQuiz(currentQuiz - 1);
 	}
@@ -30,7 +32,11 @@ export default function Quiz() {
 	return (
 		<div>
 			<h1>QUIZ</h1>
-			<Game quiz={whatquiz[currentQuiz]} score={score} nextClick={nextClick} previousClick={previousClick}/>
+			{quizzes[currentQuiz] ?
+			<Game quiz={quizzes[currentQuiz]} score={score} nextClick={nextClick} previousClick={previousClick}/>
+			:
+			<h1>Loading...</h1>
+			}
 		</div>
 	);
 }
