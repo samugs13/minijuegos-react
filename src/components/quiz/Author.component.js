a5ac0c8cd12e0e0094906d5f76e3b4800b0ec1b2
samugs13 <garciasanchezsamu@gmail.com>
2021-12-20 14:25:18 +0100

    added author component

---
+++ src/components/quiz/Author.component.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Author(props) {
+    return (
+    <div>{props.author.profilename}</div>
+    );
+}
--- src/components/quiz/Game.component.js
+++ src/components/quiz/Game.component.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Actionbar from './Actionbar.component';
 import Question from './Question.component'
 import Answer from './Answer.component'
+import Author from './Author.component'
 
 export default function Game(props) {
     return (
         <div>
             <Question question={props.quiz.question}/>
+            <Author author={props.quiz.author}/>
             <Answer />
             <Actionbar/>
         </div>
