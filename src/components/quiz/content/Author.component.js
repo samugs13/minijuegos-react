088572dbebeac26800a907618388eb97a8d25a09
Andres Cienfuegos <andres.acienfuegos@gmail.com>
2021-12-20 20:08:12 +0100

    fix: author

--- src/components/quiz/content/Author.component.js
+++ src/components/quiz/content/Author.component.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 export default function Author(props) {
+	const author = props.author;
+	const displayName = author ? author.profileName || author.username || "Unknown" : "Unknown";
+	const displayPhoto = author ? author.photo.url || "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mm&f=y" : "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mm&f=y";
+
 	return (
-		<div>{props.author.profilename}</div>
+		<div>
+			<div>
+				<img src={displayPhoto} className="profile-photo" alt="profile" />
+				<h5>Created by: {displayName}</h5>
+			</div>
+		</div>
 	);
 }
