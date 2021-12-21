f84fcbb499601ca3a3240b58e938e56078597811
samugs13 <garciasanchezsamu@gmail.com>
2021-12-21 14:32:04 +0100

    fixed score format and added second lang

--- src/components/quiz/content/Score.component.js
+++ src/components/quiz/content/Score.component.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { LangContext } from "../../lang/LangContext.component";
 
 export default function Score(props) {
 
 	return (
 		<h1>
-            <span className="badge bg-success" style={{marginLeft: "10px",}}>
-                {props.score}
-            </span>
+            <LangContext.Consumer>
+				{(context) => {
+					return (
+						<span className="badge bg-success" style={{marginLeft: "10px",}}>
+               				{context.dictionary["score"]}: {props.score}/10
+            			</span>
+					)}
+				}
+			</LangContext.Consumer>
 		</h1>
 	);
 }
+
+			
